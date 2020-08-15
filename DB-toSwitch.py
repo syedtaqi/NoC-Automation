ad8a3ed0938bfba80b2f4b9ff461cde84d01c090
syedtaqi <raza.taqi4@gmail.com>
2020-08-15 14:17:56 -0400

    Update DB-toSwitch.py

--- DB-toSwitch.py
+++ DB-toSwitch.py
@@ -1,17 +1,21 @@
+#Author: TAQI RAZA, CCNP CERTIFIED, NETWORK AUTOMATION ENGINEER
+#COPYRIGHT @2020
+
 #!/usr/bin/env python
+
+#PRE-REQUISITE: RUN SCRIPT vlan2Db.py before running this script
+#SWITCH AND DATABASE SYNCRONIZATION, USTILIZING NETWORK PROGRAMABILITY TECHNIQUES
 
 from netmiko import ConnectHandler
 import mysql.connector
-from pandas.io import sql
-import csv
-import pandas as pd
-import json
-from sqlalchemy import create_engine
-import pymysql
-from sqlalchemy.sql import alias, select
+#from pandas.io import sql
+#import csv
+#import pandas as pd
+#import json
+#from sqlalchemy import create_engine
+#import pymysql
+#from sqlalchemy.sql import alias, select
 
-
-#driver = get_network_driver('ios')
 
 hostname ="localhost"
 uname ="root"
@@ -27,192 +31,75 @@ myDB = mysql.connector.connect(
 )
 
 ##DO an IF/ELSE STATEMENT
-Vcursor = myDB.cursor()
-vardb = "INSERT INTO VLANS (VLAN, NAME,STATUS,PORT) VALUES (%s, %s, %s, %s)"
-dat = ('12', 'updatedVLAN', 'Active', ' ')
-Vcursor.execute(vardb,dat)
-myDB.commit()
-Vcursor.execute("SELECT*FROM VLANS")
-myresv = Vcursor.fetchall()
+
+forDB = raw_input("Add new VLAN Database? Y/N: ")
+
+if forDB == "Y":
+        a = raw_input("Choose VLAN ID (2-4094) :")
+        b = raw_input("Enter VLAN NAME :")
+
+        Vcursor = myDB.cursor()
+        print("Updating Database...")
+        vardb = "INSERT INTO VLANS (VLAN, NAME,STATUS,PORT) VALUES (%s, %s, %s, %s)"
+        dat = (a, b, 'Active', ' ')
+        Vcursor.execute(vardb,dat)
+        myDB.commit()
+        Vcursor.execute("SELECT*FROM VLANS")
+        myresv = Vcursor.fetchall()
 
 
-sql = "SELECT VLAN, NAME FROM VLANS WHERE VLAN = %s"
+        sql = "SELECT VLAN, NAME FROM VLANS WHERE VLAN = %s"
+        #Captures new entry as per the user input from the database, and makes fetches info to the switch
+        sql_select = (a,)
+        Vcursor.execute(sql, sql_select)
 
-sql_select = (12,)
-Vcursor.execute(sql, sql_select)
+        myresult = Vcursor.fetchone()
 
-myresult = Vcursor.fetchone()
+#Converting tuple into the string, to store in a file
+        h = myresult[0]
+        strh = ''.join(h)
 
-#Converting Tuple into Strings to store in a file
-h = myresult[0]
-strh = ''.join(h)
+        hel =  myresult[1]
+        strh1 = ''.join(hel)
 
-hel =  myresult[1]
-strh1 = ''.join(hel)
-
-straw = "Vlan "+strh +'\n'
-Joke = "Name " + strh1 +'\n'
-with open("/home/VlanAutomation/DBtoSwitch.txt", 'w') as out:
-        out.writelines([straw, Joke])
-
-###############################################################
-############# CODE IN PROGRESS!!!!
-#############
-###############################################################
-
-#for x in myresv:
-#       print(x)
-
-iosv_l2 = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.122.73',
-    'username': 'taqi',
-    'password': 'cisco',
-}
-
-net_connect = ConnectHandler(**iosv_l2)
-net_connect.find_prompt()
-#output = net_connect.send_command('show ip int brief')
-#print output
-
-VLN = raw_input('Do you want to create Vlans? Y/N ')
-if VLN == "Y":
-
-        for n in range (2,5):
-                print "Creating VLAN " + str(n)
-                config_commands = ['vlan ' + str(n), 'name Sales_VLAN'+str(n)]
-                output = net_connect.send_config_set(config_commands)
-                print output
-
-        for n in range (6,10):
-                print "Creating VLAN " + str(n)
-                config_commands = ['vlan ' + str(n), 'name Finance_VLAN'+str(n)]
-                output = net_connect.send_config_set(config_commands)
-                print output
+#Adding string infront of variables, to build a config file for vlan configuration
+#Writing lines to a file saved in a directory
+        straw = "Vlan "+strh +'\n'
+        Joke = "Name " + strh1 +'\n'
+        with open("/home/VlanAutomation/DBtoSwitch_Commands", 'w') as out:
+                out.writelines([straw, Joke])
 
 
-        outputData = net_connect.send_command('Show vlan brief')
-        #print (json.dumps(outputData, indent=3))
-#       print(outputData)
+#Opening a file
+        with open('/home/VlanAutomation/DBtoSwitch_Commands') as fopn:
+                command_to_send = fopn.read().splitlines()
+#Creating dictionary for Switch
 
-#       Vln_ = TorontovL2.get_vlans()
-#       print (json.dumps(Vln_, indent=3))
+        iosv_l2 = {
+        'device_type': 'cisco_ios',
+        'ip': '192.168.122.73',
+        'username': 'taqi',
+        'password': 'cisco',
+        }
 
-        with open('/home/VlanAutomation/ShowVlanBrief.txt', 'w' ) as Vlan_Brief1:
-                Vlan_Brief1.write(outputData)
+        all_devices =[iosv_l2]
+
+        #for loop to Update the switch as per the new entry from the Database
+        for device in all_devices:
+                net_connect = ConnectHandler(**iosv_l2)
+                vloutput = net_connect.send_config_set(command_to_send)
+        print("Updating Switch....")
+        vldata = net_connect.send_config_set("Do Show Vlan Brief")
+        print(vldata)
+
+        print("Storing Show Vlan Brief to a file....")
+        with open('/home/VlanAutomation/VLAN-Update-Log.txt', 'w' ) as Vlan_Brief1:
+                Vlan_Brief1.write(vldata)
                 Vlan_Brief1.close()
 
-
-         #This code is for database
-
-        data = pd.read_csv('/home/VlanAutomation/ShowVlanBrief.txt', skiprows=[2], names =['VLANS'])
-#       data.columns = [ "VLAN","NAME", "STATUS"]
-        print("Printing DataFrame")
-
-# bydefault splitting is done on the basis of single space.
-        dataintocolumns = data.VLANS.str.split(expand=True)
-        print(dataintocolumns)
-
- #creating engine:
-
-        engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-                                .format(host=hostname, db=dbname, user=uname, pw=pwd))
-
-        dataintocolumns.to_sql('VLANS', engine, if_exists = 'replace',index=False)
-
-
-
- #       file = open ('/home/VlanAutomation/ShowVlanBrief.txt', 'r')
- #       file_content = file.readlines()[2:]
-#       print (file_content)
-#       file.close()
-
-#       print "Syncing Database with Switch... "
-#       query = ("INSERT INTO VLANS (ID, NAME, STATUS) VALUES (%s, %s, %s)")
-
-        #Split() method is used to splits a string into a list
-#       values = [line.split('') for line in file_content
-
-#       query = ("INSERT INTO VLANS (ID, NAME, DESCRIPTION) VALUES (%s, %s, %s)")
-#       Vcursor.executemany(query,values)
-#       mydb.commit()
-#       Vcursor.execute ("SELECT*FROM VLANS")
-
-#       result = cursor.fetchall()
-#       for x in result:
-#               print(x)
-
-else :
-        print 'No Vlan Added'
-
-DELVLN = raw_input('Do you want to delete Vlans? Y/N?')
-
-if DELVLN == "Y" :
-        DELVLN2 = raw_input('1. 2-4 or 2. 6-9? ' )
-        if DELVLN2 == '1':
-                for b in range (2,5):
-                        print "Deleting Vlans from 2-5"
-                        config_commands = [ 'no vlan ' + str(b)]
-                        output1 = net_connect.send_config_set(config_commands)
-                        print output1
-        elif DELVLN2 == '2' :
-                for c in range (6,10):
-                        print "Deleting Vlans from 6-10"
-                        config_commands = ['no vlan ' + str(c)]
-                        output2 = net_connect.send_config_set(config_commands)
-                        print output2
-        DataDelete = net_connect.send_command('Show vlan brief')
-#       print DataDelete
-
-
-        with open('/home/VlanAutomation/DeletedVlans.txt', 'w') as Del_Vlan_DB:
-                Del_Vlan_DB.write(DataDelete)
-                Del_Vlan_DB.close()
-
-        dataD = pd.read_csv('/home/VlanAutomation/DeletedVlans.txt', skiprows=[2], names =['VLANS'])
-#       data.columns = [ "VLAN","NAME", "STATUS"]
-        print("Printing DataFrame")
-
-# bydefault splitting is done on the basis of single space.
-        dataintocolumnsD = dataD.VLANS.str.split(expand=True)
-        print(dataintocolumnsD)
-
- #creating engine:
-
-        engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-                                .format(host=hostname, db=dbname, user=uname, pw=pwd))
-
-        dataintocolumnsD.to_sql('VLANS', engine, if_exists = 'replace',index=False)
-
-
-
-elif DELVLN =="N" :
-        print "Vlans not delete"
-
-else :
-    print ("Non")
-
-
-### USE CASE: UPDATE ENTRY IN MYSQL, and Configure SWITCH with SYNCED ENTRIES
-
-
-
-UPDB = raw_input("Do you want to UPDATE your Database Y/N?")
-
-if UPDB == 'Y':
-
-        print("Updating  VLAN Entry in DB:NETWORK..")
-        engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-                                .format(host=hostname, db=dbname, user=uname, pw=pwd))
-
-        engine.execute('INSERT INTO VLANS(0, 1, 2) VALUES ("14","UpdateVLAN", "Active")')
-
-
-
-        result = engine.execute('SELECT * FROM VLANS')
-        for _r in result:
-                 print(_r)
-
-
 else:
-        print("non")
+        print "No Vlan Added"
+        print "Exit"
+
+###End of Code
+
